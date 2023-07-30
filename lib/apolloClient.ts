2317e8b93005ff33bd40ac532dd00d23d343fc91
noko206 <61347127+noko206@users.noreply.github.com>
2023-07-30 22:44:31 +0900

    Protection of Hasura endpoint

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -6,14 +6,15 @@ import {
 } from '@apollo/client'
 import 'cross-fetch/polyfill'
 
-export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__'
-
 let apolloClient: ApolloClient<NormalizedCacheObject> | undefined
 const createApolloClient = () => {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
     link: new HttpLink({
       uri: 'https://hasura-noko206.hasura.app/v1/graphql',
+      headers: {
+        'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_KEY,
+      },
     }),
 
     cache: new InMemoryCache(),
